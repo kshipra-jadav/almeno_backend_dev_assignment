6af5dec25cc626ba920840ef0ef6806b07e70373
kshipra-jadav <jadavkshipra2@gmail.com>
2023-06-29 07:40:30 +0530

    initialized client with vite, react, ts and tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+// import './index.css'
+function App() {
+
+  return (
+    <>
+      <div className="text-6xl text-center">
+        Urine Strip Analyzer
+      </div>
+    </>
+  )
+}
+
+export default App
