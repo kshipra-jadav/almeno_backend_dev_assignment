88def737210ebf9f6f2d8eda6bf64f5503b50c59
kshipra-jadav <jadavkshipra2@gmail.com>
2023-06-29 15:52:11 +0530

    client: fix cors error

--- client/src/utils/axiosConfig.ts
+++ client/src/utils/axiosConfig.ts
@@ -3,7 +3,4 @@ import { SERVER_BASE_URL } from "./constants.ts";
 
 export const axiosInstance = axios.create({
   baseURL: SERVER_BASE_URL,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
 });
